a1d4d11b05b8c815e11ffc07d97beb8a20f4eb40
fadjar.iftikhar <fadjar.iftikhar@gmail.com>
2020-01-13 07:46:48 -0500

    github-finder init

---
+++ react-front-to-back/github-finder/src/App.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import './App.css';
+
+//=================================
+//Class Component
+//=================================
+class App extends React.Component {
+  render() {
+    return (
+       <div className="App">
+          <h1>it's fine</h1>'
+        </div>
+    );
+  }
+}
+
+//=================================
+//Function Component
+//=================================
+// function App() {
+//   return (
+//     <div className="App">
+//      <h1>it's fine</h1>'
+//     </div>
+//   );
+// }
+
+export default App;
